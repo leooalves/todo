0919748c0559adfcd0dc49f872e21316035bf6d9
Leonardo Oliveira <leonardo.oliveiralves@gmail.com>
2021-05-25 15:44:27 -0300

    Manipulacao do forms e iteracao da pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+	public todos: Todo[] = [];
+	public title: String = "Minhas tarefas";
+	public form: FormGroup;
+
+
+	constructor(private fb: FormBuilder) {
+		this.form = this.fb.group({
+			title: ['', Validators.compose([
+				Validators.minLength(5),
+				Validators.maxLength(60),
+				Validators.required
+			])]
+		});
+		this.load();
+	}
+
+	add(){
+		const title = this.form.controls["title"].value;
+		const id = this.todos.length +1;
+		this.todos.push(new Todo(id,title,false))
+		this.save();
+		this.clear();
+	}
+
+	clear(){
+		this.form.reset();
+	}
+
+	remove(todo: Todo){
+		const index = this.todos.indexOf(todo);
+		if(index !== -1){
+			this.todos.splice(index,1);
+		}
+	}
+
+	markAsDone(todo: Todo){
+		todo.done = true;
+	}
+
+	markAsUndone(todo: Todo){
+		todo.done = false;
+	}
+
+	save(){
+		const data = JSON.stringify(this.todos);
+		localStorage.setItem("todos",data);
+	}
+
+	load(){
+		const data = localStorage.getItem("todos");
+		this.todos = data !== null ? JSON.parse(data):[];
+	}
 }
